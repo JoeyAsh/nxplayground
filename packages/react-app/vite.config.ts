3481b1cacb98bc7c70ee94e1929dd5d86dfc5663
Johannes Aschenbrenner <johannes.aschenbrenner@dormakaba.com>
2024-12-03 11:52:04 +0100

    remove dts from react-app

--- packages/react-app/vite.config.ts
+++ packages/react-app/vite.config.ts
@@ -8,11 +8,7 @@ export default defineConfig({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/packages/react-app',
   plugins: [
-    react(),
-    dts({
-      entryRoot: 'src',
-      tsconfigPath: join(__dirname, 'tsconfig.app.json'),
-    }),
+    react()
   ],
   resolve: {
     alias: [
